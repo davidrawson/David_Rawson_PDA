6205819506c034fcc2a1476ef7414f468e4b65ff
David Rawson <dm.rawson@gmail.com>
2018-04-05 20:16:24 +0100

    Unit and integration testing done.

--- Coding_Exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Coding_Exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,20 +11,28 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-  // it('it can add two numbers', function(){
-  //
-  // })
+  it('it can add two numbers', function(){
+    calculator.previousTotal = 1
+    calculator.add(4)
+    assert.equal(5, calculator.runningTotal)
+  })
 
-  // it('it can subtract two numbers', function(){
-  //
-  // })
+  it('it can subtract two numbers', function(){
+    calculator.previousTotal = 7
+    calculator.subtract(4)
+    assert.equal(3, calculator.runningTotal)
+  })
 
-  // it('it can multiply two numbers', function(){
-  //
-  // })
+  it('it can multiply two numbers', function(){
+    calculator.previousTotal = 3
+    calculator.multiply(5)
+    assert.equal(15, calculator.runningTotal)
+  })
 
-  // it('it can divide two numbers', function(){
-  // 
-  // })
+  it('it can divide two numbers', function(){
+    calculator.previousTotal = 21
+    calculator.divide(7)
+    assert.equal(3, calculator.runningTotal)
+  })
 
 });
